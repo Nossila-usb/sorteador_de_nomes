73589ee9858b78675241de19ae56e0c91ec6dd81
Nossila-usb <minimonik1909@gmail.com>
2024-08-23 15:28:30 -0300

    versao 1.0

---
+++ sorteio.py
@@ -0,0 +1,27 @@
+import random
+
+nomes = []
+
+while True:
+    print('1 - Inserir na lista.')
+    print('2 - Sortear.')
+    print('3 - Sair do programa.')
+
+    opcao = input('Opção desejada: ')
+    match opcao:
+       case '1':
+         nome =  input('Insira o nome: ')
+         nomes.append(nome)
+         print(f'{nome} Inserido com secesso.') 
+         continue
+
+       case '2':
+        nome_sorteado = random.randint(0, len(nomes)-1)
+        print(f'nome soteado {nomes[nome_sorteado]}')
+        continue
+       case '3':
+         print('Progama encerrado.')
+         break
+       case _:
+          print('Opção invalida!')
+
